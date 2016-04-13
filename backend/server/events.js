2c0f8c9140191f976da773c398d4ec4bf205698d
Steven Chien <steven.chien@connect.polyu.hk>
2016-04-13 18:56:19 +0800

    Added event search by hotspot

---
+++ backend/server/events.js
@@ -0,0 +1,6 @@
+Api.addRoute('events_by_spot/:name', { authRequired: false }, {
+	get: function () {
+		var name = this.urlParams.name;
+		return Events.find({ spot: name }, { fields: { 'name': 0, '_id': 0 } }).fetch();
+	}
+});
