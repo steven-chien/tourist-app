e19d9cbfe4432c0e12044e50f3539a056c39c865
Chi Kuen Yau <Chi Kuen Yau>
2016-04-14 21:50:07 +0800

    fix on merging

--- app/app/src/main/java/hk/edu/polyu/comp/hellohongkong/app/MainActivity.java
+++ app/app/src/main/java/hk/edu/polyu/comp/hellohongkong/app/MainActivity.java
@@ -1,64 +1,80 @@
 package hk.edu.polyu.comp.hellohongkong.app;
 
 import android.content.Intent;
-import android.os.Bundle;
+import android.media.Image;
+import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private final int BACKGROUND_MIN = 0;
+    private final int BACKGROUND_MAX = 3;
+
+    private ServerRequestManager svServerRequestManager;
+    private TourSpotManager svTourSpotManager;
+
+    private ImageView mvBackgroundImageContainer;
+    private FloatingActionButton mvMainMapButton;
+    private FloatingActionButton mvMainHotSpotButton;
+    private FloatingActionButton mvMainEventButton;
+    private FloatingActionButton mvMainNFCButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hello_world);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_main);
+        svServerRequestManager = ServerRequestManager.getInstance();
+        svTourSpotManager = TourSpotManager.getInstance();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+//        mvBackgroundImageContainer = (ImageView) findViewById(R.id.mainBackgroundContainer);
+//        Random lvRandom = new Random();
+//        int lvRandomBackgroundIndex = lvRandom.nextInt((BACKGROUND_MAX - BACKGROUND_MIN) + 1) + BACKGROUND_MIN;
+//        mvBackgroundImageContainer.setImageResource(getResources().getIdentifier("main_background" + lvRandomBackgroundIndex, "drawable", getPackageName()));
+
+        mvMainMapButton = (FloatingActionButton) findViewById(R.id.mainMapButton);
+        mvMainMapButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, TourHotSpotMapActivity.class);
+                startActivity(intent);
             }
         });
+
+        mvMainHotSpotButton = (FloatingActionButton) findViewById(R.id.mainHotSpotButton);
+        mvMainHotSpotButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HotSpotInfo.class);
+                startActivity(intent);
+            }
+        });
+
+        mvMainEventButton = (FloatingActionButton) findViewById(R.id.mainEventButton);
+        mvMainEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, TourSpotDetailActivity.class);
+//                startActivity(intent);
+            }
+        });
+
+        mvMainNFCButton = (FloatingActionButton) findViewById(R.id.mainNFCButton);
+        mvMainNFCButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, TourSpotDetailActivity.class);
+//                startActivity(intent);
+            }
+        });
+
+
+        // Request Hot Spot Info
+        svServerRequestManager.requestHotSpotInfo();
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_hello_world, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void hotSpotButtonClick(View view) {
-        Intent i = new Intent(getApplicationContext(), HotSpotInfo.class);
-        startActivity(i);
-    }
-
-    public void eventsButtonClick(View view) {
-        Intent i = new Intent(getApplicationContext(), EventsInfo.class);
-        startActivity(i);
-    }
-
 }
