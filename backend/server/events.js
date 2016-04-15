92fcb2b47b5d0800d29c0dba90913a2b20b4e7d1
Steven Chien <steven.chien@connect.polyu.hk>
2016-04-15 17:34:54 +0800

    Fixed int issue in search event by id

--- backend/server/events.js
+++ backend/server/events.js
@@ -7,7 +7,7 @@ Api.addRoute('events_by_spot/:name', { authRequired: false }, {
 
 Api.addRoute('events_by_id/:name', { authRequired: false }, {
 	get: function () {
-		var name = this.urlParams.name;
+		var name = parseInt(this.urlParams.name);
 		return Events.find({ id: name }, { fields: { '_id': 0 } }).fetch();
 	}
 });
